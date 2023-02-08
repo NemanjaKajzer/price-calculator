a63687a7495f9009a9b44270cc2992b67d1f5506
NemanjaKajzer <kajzernemanja@gmail.com>
2023-02-08 15:51:53 +0100

    Moved price report calculations from methods

--- SharpenSkills.Logic/PriceReport/PriceReport.cs
+++ SharpenSkills.Logic/PriceReport/PriceReport.cs
@@ -6,32 +6,10 @@
         public Money TaxTotal { get; private set; }
         public Money Total { get; private set; }
 
-        public PriceReport()
-        {
-            Price = new Money();
-            TaxTotal = new Money();
-            Total = new Money();
-        }
-
         public PriceReport(Product product, ITax tax)
         {
-            SetProduct(product);
-            SetTaxTotal(product, tax);
-            SetTotal();
-        }
-
-        public void SetProduct(Product product)
-        {
             Price = product.Price;
-        }
-
-        public void SetTaxTotal(Product product, ITax tax)
-        {
             TaxTotal = product.Price * tax.Percentage;
-        }
-
-        private void SetTotal()
-        {
             Total = Price + TaxTotal;
         }
     }
